f15bc4b96cdaf8d6ec1ebe8aed6cb6b739f72c9c
stukene <estuken53@gmail.com>
2020-07-27 22:29:00 -0700

    quick algo

--- removeElement.js
+++ removeElement.js
@@ -1,2 +1,12 @@
 var removeElement = function(nums, val) {
+    const len = nums.lenght();
+    let index;
+    for(let i = 0; i < len; i++) {
+        if(nums[i] === val) {
+            index = i;
+            break;
+        }
+    }
+
+    return nums.splice(index, index)
 };
