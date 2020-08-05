fb51559218a5ec051de0c40177de90540e711402
stukene <estuken53@gmail.com>
2020-08-04 20:02:23 -0700

    algo finished

--- combiningSums.js
+++ combiningSums.js
@@ -1,3 +1,23 @@
 var combinationSum = function(candidates, target) {
+    let res = [];
+    helper(target, 0, candidates, [], res);
+    console.log(res);
+    return res;
     
 };
+
+function helper(target, start, candidates, sol, res) {
+    if (target < 0) { return; }
+
+    if (target == 0) {
+        let temp = sol;
+        res.push(temp.slice());
+        return;
+    }
+
+    for (let i = start; i < candidates.length; i++) {
+        sol.push(candidates[i]);
+        helper(target - candidates[i], i, candidates, sol, res);
+        sol.pop();
+    }
+}
