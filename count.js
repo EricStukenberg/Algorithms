72a5897834f7f9767001fd774b7d612b2fe290f7
stukene <estuken53@gmail.com>
2020-08-06 11:56:24 -0700

    recursive

--- count.js
+++ count.js
@@ -1,19 +1,10 @@
 var countAndSay = function(n) {
     if (n === 1) return '1';
-    if (n === 2) return '11';
-    let result = '11';
-    for (let k = 2; k < n; k++) {
-      let temp = '', count = 1;
-      for (let i = 0; i < result.length - 1; i++) {
-        if (result[i] === result[i+1]) {
-          count++;
-        } else {
-          temp += count + '' + result[i];
-          count = 1;
-        }
-      }
-      temp += count + '' + result[result.length - 1];
-      result = temp;
-    }
-    return result;
+    let result = '';
+    const prev = countAndSay(n - 1)
+    
+    for (let o = 0, i = 1; i <= prev.length; i++)
+        prev[i] !== prev[o] && (result += `${i - o}${prev[o]}`, o = i)
+
+    return result
 };
