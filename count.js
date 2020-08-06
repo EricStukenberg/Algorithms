0a5e17b557db20ee51a77ba99e0d5bc0e6b39bb5
stukene <estuken53@gmail.com>
2020-08-05 17:48:41 -0700

    Sol

--- count.js
+++ count.js
@@ -1,3 +1,19 @@
 var countAndSay = function(n) {
-    
+    if (n === 1) return '1';
+    if (n === 2) return '11';
+    let result = '11';
+    for (let k = 2; k < n; k++) {
+      let temp = '', count = 1;
+      for (let i = 0; i < result.length - 1; i++) {
+        if (result[i] === result[i+1]) {
+          count++;
+        } else {
+          temp += count + '' + result[i];
+          count = 1;
+        }
+      }
+      temp += count + '' + result[result.length - 1];
+      result = temp;
+    }
+    return result;
 };
