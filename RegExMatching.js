7ba357742b61a468efcbd5e8675af5f91d46415c
EricStukenberg <42079088+EricStukenberg@users.noreply.github.com>
2020-08-26 17:02:24 -0700

    First attempt almost done

--- RegExMatching.js
+++ RegExMatching.js
@@ -1,1 +1,22 @@
 
+var isMatch = function(s, p) {
+    let res = true
+    
+    for(let i = 0; i< s.length; i++) {
+        checkSubString(subString, p)
+    }
+    return res;
+};
+
+function checkSubString(subString, p) {
+    let res = true;
+    
+    for(let i = 0; i< p.length; i++) {
+        if(s[i] !== p[i] && p[i] !== '.') {
+
+        } else if(p[i] === '*' && i < 0) {
+
+        }
+    }
+    return res;
+}
