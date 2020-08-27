0d9dba562e1f6c9a051d387d3c7bc8940d271da4
EricStukenberg <42079088+EricStukenberg@users.noreply.github.com>
2020-08-26 17:11:31 -0700

    Mixed Up Vars Need to fix

--- RegExMatching.js
+++ RegExMatching.js
@@ -1,22 +1,30 @@
-
 var isMatch = function(s, p) {
-    let res = true
-    
-    for(let i = 0; i< s.length; i++) {
-        checkSubString(subString, p)
+    let res = false
+    if(p.length < s.length) {
+        return false
+    }
+    for(let i = 0; i< p.length - s.length; i++) {
+            let subP = s.substring(i, s.length+i)
+            res = checkSubString(subP, s)
+        
+        if(res === true) {
+            return res;
+        }
     }
     return res;
 };
 
-function checkSubString(subString, p) {
-    let res = true;
-    
+function checkSubString(s, p) {    
     for(let i = 0; i< p.length; i++) {
         if(s[i] !== p[i] && p[i] !== '.') {
-
-        } else if(p[i] === '*' && i < 0) {
-
-        }
+          if(p[i] === '*' && i < 0) {
+              if(p[i-1] !== s[i]) {
+                  return false
+              }
+          } else {
+              return false;
+          }
+        } 
     }
-    return res;
+    return true;
 }
