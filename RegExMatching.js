e68f8d94cc044b782c4dc3f1325a877ee5467f95
EricStukenberg <42079088+EricStukenberg@users.noreply.github.com>
2020-08-27 17:23:43 -0700

    Update RegExMatching.js

--- RegExMatching.js
+++ RegExMatching.js
@@ -1,10 +1,16 @@
 var isMatch = function(s, p) {
+    return (new RegExp('^'+p+'$')).test(s);
     let res = false
     if(p.length < s.length) {
+        console.log("False Return im")
         return false
     }
-    for(let i = 0; i< p.length - s.length; i++) {
-            let subP = s.substring(i, s.length+i)
+    for(let i = 0; i < p.length - s.length+1; i++) {
+            console.log(s + " p = " + p)
+
+            let subP = p.substring(i, s.length+i)
+                        console.log(s + " p = " + p)
+
             res = checkSubString(subP, s)
         
         if(res === true) {
@@ -14,13 +20,16 @@ var isMatch = function(s, p) {
     return res;
 };
 
-function checkSubString(s, p) {    
+function checkSubString(p, s) {    
+    console.log("subP= "+ p + " s = " + s)
     for(let i = 0; i< p.length; i++) {
         if(s[i] !== p[i] && p[i] !== '.') {
-          if(p[i] === '*' && i < 0) {
-              if(p[i-1] !== s[i]) {
+            console.log("s[i] = " + s[i] + " p[i] = " + p[i] + " i=" + i)
+          if(p[i] === '*' && i > 0) {
+              if(p[i-1] !== s[i] && p[i-1] !== '.') {
                   return false
               }
+              
           } else {
               return false;
           }
