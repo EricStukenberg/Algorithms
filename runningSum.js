b6f5cf4fa4db83f57b9c2f5b9f7755b0325e2298
EricStukenberg <EricStukenberg@users.noreply.github.com>
2020-09-25 18:22:36 -0700

    inside loop

--- runningSum.js
+++ runningSum.js
@@ -2,6 +2,8 @@ var runningSum = function(nums) {
     let sum = 0
     const res = []
     for(let i = 0; i < nums.length; i++) {
+        sum += nums[i]
+        res.push(sum)
         
         
     }
