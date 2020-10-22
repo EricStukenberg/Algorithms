5d04b5d0c523dfcda7ebb8bc52bd3b4965668466
EricStukenberg <42079088+EricStukenberg@users.noreply.github.com>
2020-10-22 15:32:19 -0700

    Update LetterComboPhone.js

--- LetterComboPhone.js
+++ LetterComboPhone.js
@@ -1,5 +1,13 @@
 const letterMap = new Map([["2", ["a", "b", "c"]], ["3", ["d", "e", "f"]], ["4", ["g", "h", "i"]], ["5", ["j", "k", "l"]], ["6", ["m", "n", "o"]], ["7", ["p",
 "q", "r","s"]], ["8", ["t", "u", "v"]], ["9", ["w", "x", "y", "z"]]])
 letterCombinations = (digits) => {
+    const lenDigit = digits.length
+    if (lenDigit <= 1 ) {
+        if(lenDigit === 1 ) {
+            return letterMap.get(digits)
+        } else {
+            return []
+        }
+    }
     
 };
