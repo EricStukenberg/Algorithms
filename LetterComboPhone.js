ae8fabb529713c06e1392255291328a687b92120
EricStukenberg <42079088+EricStukenberg@users.noreply.github.com>
2020-10-22 15:56:58 -0700

    Update LetterComboPhone.js

--- LetterComboPhone.js
+++ LetterComboPhone.js
@@ -1,20 +1,34 @@
 const letterMap = new Map([["2", ["a", "b", "c"]], ["3", ["d", "e", "f"]], ["4", ["g", "h", "i"]], ["5", ["j", "k", "l"]], ["6", ["m", "n", "o"]], ["7", ["p",
 "q", "r","s"]], ["8", ["t", "u", "v"]], ["9", ["w", "x", "y", "z"]]])
+
 letterCombinations = (digits) => {
-    const lenDigit = digits.length
-    if (lenDigit <= 1 ) {
-        if(lenDigit === 1 ) {
+    const digitLen = digits.length
+    if (digitLen <= 1 ) {
+        if(digitLen === 1 ) {
             return letterMap.get(digits)
         } else {
             return []
         }
     }
     let res = []
-    let digitSet = getDigitSet(digits, lenDigit)
-    let firstSetLen = digitSet[0].length
-    temp = ''
-};
+    let digitSet = getDigitSet(digits, digitLen)
+    let temp = ''
+    backtrack(temp, digitSet, 0, digitLen)
 
+    function backtrack(temp, data, currLev, endLev) {
+        if (temp.length == endLev) {
+            res.push(temp)
+            return
+        }
+        for(let i = 0; i < data[currLev].length; i++) {
+            temp += data[currLev][i]
+            backtrack(temp, data, currLev+1, endLev)
+            temp = temp.slice(0, temp.length-1)
+        }
+    }
+    
+    return res;
+};
 
 const getDigitSet = (digits, lenDigit) => {
         let res = []
