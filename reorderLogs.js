783639cfd19a507c9d4ebe261371aa00e9b4af88
EricStukenberg <42079088+EricStukenberg@users.noreply.github.com>
2020-10-30 12:39:34 -0700

    Update reorderLogs.js

--- reorderLogs.js
+++ reorderLogs.js
@@ -1,10 +1,12 @@
-
 var reorderLogFiles = function(logs) {
     let len = logs.length
     let res = []
     
     for(let i = 0; i < len; i++) {
         let wordArr = logs[i].split(" ")
+        if(isNaN(wordArr[wordArr.length-1])) {
+            
+        }
     }
     
 };
