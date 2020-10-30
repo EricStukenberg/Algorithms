bd6bc611fcd8ef8a16656dd089be51edee2e934c
EricStukenberg <42079088+EricStukenberg@users.noreply.github.com>
2020-10-30 12:36:49 -0700

    Update reorderLogs.js

--- reorderLogs.js
+++ reorderLogs.js
@@ -1,1 +1,10 @@
 
+var reorderLogFiles = function(logs) {
+    let len = logs.length
+    let res = []
+    
+    for(let i = 0; i < len; i++) {
+        let wordArr = logs[i].split(" ")
+    }
+    
+};
