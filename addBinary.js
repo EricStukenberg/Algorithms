8be25b89eb5ab7945570e035ad22cfe4be808296
EricStukenberg <42079088+EricStukenberg@users.noreply.github.com>
2020-11-06 13:11:36 -0800

    Update addBinary.js

--- addBinary.js
+++ addBinary.js
@@ -1,4 +1,13 @@
 
 addBinary = (a, b) => {
+    let aNum = 0
+    let bNum = 0
+    //get a 
+    for(let i = 0; i < a.length; i++) {
+        if(a.charAt(i) == 1) {
+            aNum += Math.pow(2, i)
+        }
+    }
+    
     
 };
