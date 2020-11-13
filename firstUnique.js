4984ee76ceb2367868a751892868182212eaaa51
EricStukenberg <42079088+EricStukenberg@users.noreply.github.com>
2020-11-13 15:55:54 -0800

    Update firstUnique.js

--- firstUnique.js
+++ firstUnique.js
@@ -1,7 +1,12 @@
 const firstUniqChar = (s) => {
     let map = {}
     for(let i = 0; i < s.length; i++) {
-        map[s.charAt(i)] += 1
+        if(map[s.charAt(i)]) {
+            map[s.charAt(i)] += 1
+
+        } else {
+            map[s.charAt(i)] = 1
+        }
     }
     for(let i = 0; i < s.length; i++) {
         if(map[s.charAt(i)] == 1) {
