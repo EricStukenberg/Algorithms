5247d474de23de593b86c13ca8b9b37469d3a1a0
EricStukenberg <42079088+EricStukenberg@users.noreply.github.com>
2020-11-20 15:21:34 -0800

    Update merge.js

--- merge.js
+++ merge.js
@@ -1,4 +1,19 @@
-
 const merge = (nums1, m, nums2, n) => {
+    let res = new Array(m + n);
+    let i = 0
+    let j = 0
+    let k = 0
     
+    while(i < m && j < n) {
+        
+        if(nums1[i] < nums2[j]) {
+            res[k] = num1[i]
+            k++
+            i++
+        } else {
+            res[k] = num2[j]
+            k++
+            j++
+        }
+    }
 };
