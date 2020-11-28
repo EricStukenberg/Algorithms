736f5712f8be7b4b2737b50e249f0d85f1085819
EricStukenberg <42079088+EricStukenberg@users.noreply.github.com>
2020-11-27 16:04:36 -0800

    Update StockSell.js

--- StockSell.js
+++ StockSell.js
@@ -3,28 +3,12 @@
  * @return {number}
  */
 const maxProfit = (prices) => {
-    let prof = 0
-    let len = prices.length
-    let buyPrice = prices[0]
-    let buyIndex = 0
-    for(let i = 0; i < len-1; i++) {
-        if(buyPrice > prices[i]) {
-            buyPrice = prices[i]
-            buyIndex = i
-        }
-    }
-    let sellIndex = buyIndex + 1
-    let sellPrice = prices[buyIndex+1] 
-    for(let i = buyIndex+1; i < len; i++) {
-        if(sellPrice > prices[i]) {
-            sellPrice = prices[i]
-        }
-    }
-    prof = buyPrice - sellPrice
-    if(prof < 0) {
-        return 0
-    } 
+    let maxProf = 0, curr = prices[0];
     
-    return prof
+    for(let x of prices) {
+        if(curr < x) maxProf = Math.max(maxProf, x - curr);
+        else curr = x;
+    }
+    return maxProf;
     
 };
