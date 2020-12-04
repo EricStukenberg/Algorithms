c16456eb678ce8f336ce69909f08f055369da328
EricStukenberg <42079088+EricStukenberg@users.noreply.github.com>
2020-12-04 13:49:33 -0800

    Update numIslands.js

--- numIslands.js
+++ numIslands.js
@@ -3,29 +3,29 @@ const numIslands = (grid) => {
     
     for(let i=0; i <grid.length; i++) {
         for(let j=0; j < grid[0].length; j++) {
-             if(grid[i][j] == 1) {
+            if(grid[i][j] == 1) {
                 islands++
-             
+                checkIsland(grid, i, j)
             }
         }
     }
+    return islands
 };
 
 const checkIsland = (grid, i, j) => {
     grid[i][j] = 0
     
     if(i > 0 && grid[i-1][j] == 1) {
-        
+        checkIsland(grid, i-1, j)
     }
     if(i <= grid.length -2 && grid[i+1][j] == 1){
-        expand(grid, i+1, j)
+        checkIsland(grid, i+1, j)
     }
     if(j> 0 && grid[i][j-1] == 1){
-        expand(grid, i, j-1)
+        checkIsland(grid, i, j-1)
     }
     if(j <= grid[0].length - 2 && grid[i][j+1] == 1){
-        expand(grid, i, j+1)
+        checkIsland(grid, i, j+1)
     }
-    
     
 }
