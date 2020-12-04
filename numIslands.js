f5ab983a298469dedbc80fd5c7136736dec38254
EricStukenberg <42079088+EricStukenberg@users.noreply.github.com>
2020-12-04 13:43:21 -0800

    Update numIslands.js

--- numIslands.js
+++ numIslands.js
@@ -1,3 +1,9 @@
 const numIslands = (grid) => {
+    let islands = 0
     
+    for(let i=0; i <grid.length; i++) {
+        for(let j=0; j < grid[0].length; j++) {
+            
+        }
+    }
 };
